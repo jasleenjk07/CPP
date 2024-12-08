7b3aac84b17e65e733ca3da0e42e617a2cabe2aa
Jasleen Kaur <kaurjasleen6104@gmail.com>
2024-12-09 00:30:42 +0530

    Update 01_InputAndDisplay.cpp

--- Classes And Objects/01_InputAndDisplay.cpp
+++ Classes And Objects/01_InputAndDisplay.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 class Input{
-    string name;
-    int x;
+    string name;        //Stores a name
+    int x;              //Stores a number
 public:
-    Input(){
+    Input(){            //Constructor to take input
         cin >> name;
         cin >> x;
     }
 
-    void display(){
+    void display(){    //Displays the stored data
         cout << x << ": " << name;
     }
 };
 
 int main(){
-    Input in;
-    in.display();    
+    Input in;         //Create an Input object
+    in.display();     //Display the input data
     return 0;
 }
